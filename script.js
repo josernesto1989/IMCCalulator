56f44574b6d33e846adc9d7a1d9eae17351c723e
josernesto1989 <josernesto1989@gmail.com>
2025-06-12 23:28:38 -0600

    ADD: Support meter and cm

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+let heightMultiply = {
+    'm': 1,
+    'cm': 100,
+}
+
+console.log('tet');
+let heightUnit = 'meter';
+
+let heightChange = function() {
+    
+    const height = parseFloat(this.value);
+    if (height < 3) {
+        heightUnit = 'm'
+    }
+    else{
+        heightUnit = 'cm';
+    }
+    document.getElementById('height-label').innerText = `Altura (${heightUnit}):`;
+};
+
+document.getElementById('height').addEventListener('change', heightChange);
+document.getElementById('height').addEventListener('keyup', heightChange);
+
+
 document.getElementById('imcForm').addEventListener('submit', function(event) {
             event.preventDefault();
             const weight = parseFloat(document.getElementById('weight').value);
             const height = parseFloat(document.getElementById('height').value);
-            const imc = weight / (height * height);
+            const divisor = heightMultiply[heightUnit];
+            const imc = weight / (height * height/(divisor*divisor));
             document.getElementById('result').innerText = `Tu IMC es: ${imc.toFixed(2)}`;
         });
