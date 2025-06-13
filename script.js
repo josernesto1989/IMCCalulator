8cb585828479380b54fda0d874b741220dcbb664
josernesto1989 <josernesto1989@gmail.com>
2025-06-13 13:03:28 -0600

    ADD: color scale

--- script.js
+++ script.js
@@ -29,4 +29,26 @@ document.getElementById('imcForm').addEventListener('submit', function(event) {
             const divisor = heightMultiply[heightUnit];
             const imc = weight / (height * height/(divisor*divisor));
             document.getElementById('result').innerText = `Tu IMC es: ${imc.toFixed(2)}`;
+            document.getElementById('result-container').removeAttribute('hidden');
+            console.log(`aca IMC: ${imc}`);
+            document.getElementById('scale-pointer').style.marginLeft=(`${(imc-16) * 10-10}px`); 
+            console.log(`aca IMC fin: ${imc}`);           
         });
+
+        const slider = document.querySelector('.slider');
+
+const options = document.querySelectorAll('.option');
+
+let selectedIndex = 0;
+const genderValue={0: 'Hombre', 1: 'Mujer'};
+options.forEach((option, index) => {
+  option.addEventListener('click', () => {
+    selectedIndex = index;
+    console.log(`Selected index: ${genderValue[selectedIndex]}`);
+    updateSlider();
+  });
+});
+
+function updateSlider() {
+  slider.style.transform = `translateX(${selectedIndex * 100}px)`;
+}
